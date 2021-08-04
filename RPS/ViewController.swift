6487c21c78e7c6585e4daca0feedaf3c19b67a88
teenagelove <dani4upa586@gmail.com>
2021-08-04 16:07:47 +0500

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Данил Казаков on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
